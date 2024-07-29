88c4ee26c8c1c4961a9541498e784baf20985709
Siddhartha Das <siddhartha@turbot.com>
2024-07-29 13:23:05 +0530

    Fix pipes connection test. #25

--- pipes/resource_pipes_connection_test.go
+++ pipes/resource_pipes_connection_test.go
@@ -283,8 +283,8 @@ func testCheckJSONString(name, key, value string) resource.TestCheckFunc {
 		}
 
 		if v, ok := rs.Primary.Attributes[key]; !ok {
-			configString, _ := formatConnectionJSONString("aws", v)
-			compareValue, _ := formatConnectionJSONString("aws", value)
+			configString, _ := formatConnectionJSONString(v)
+			compareValue, _ := formatConnectionJSONString(value)
 			if configString != compareValue {
 				if emptyCheck && !ok {
 					return nil
